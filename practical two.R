1d891088344a9c843751ef10bdc1a65a394664a5
Selaelo Kgafela <kgfsel001@myuct.ac.za>
2025-02-08 10:48:23 +0200

    Data and Span have been defined

--- practical two.R
+++ practical two.R
@@ -1,2 +1,16 @@
-#Lowes
-fdfs
+#Generating simulated data
+set.seed(1)
+length<-100
+X<-(1:length)
+error_terms<-rnorm(100,0,02^2)
+error_terms
+Y_matrix<-matrix(error_terms)
+
+for(i in 1:length){
+  
+  Y_matrix[i]<-sin(X[i]/10)+Y_matrix[i]
+}
+# span 
+k<-ceiling(0.021*length)
+k
+
