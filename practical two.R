d675c74fae6afd1853c6ecd492d37ed3650f5c7e
Selaelo Kgafela <kgfsel001@myuct.ac.za>
2025-02-08 15:31:35 +0200

    close to determining teh weight of one X

--- practical two.R
+++ practical two.R
@@ -1,7 +1,7 @@
 #Generating simulated data
 set.seed(1)
 length<-100
-X<-(1:length)
+X<-matrix(1:length)
 error_terms<-rnorm(100,0,02^2)
 error_terms
 Y_matrix<-matrix(error_terms)
@@ -11,6 +11,16 @@ for(i in 1:length){
   Y_matrix[i]<-sin(X[i]/10)+Y_matrix[i]
 }
 # span 
-k<-ceiling(0.021*length)
-k
+k<-ceiling(0.021*length)+1#should be put inside function
+#-----
+X[1]
+distances<-matrix(NA,nrow=nrow(X),ncol=2) #col2 is index of distance
 
+for (i in 1:nrow(X)){
+  distances[i,1]<-abs(X[1]-X[i])
+  distances[i,2]<-X[i]
+}
+sorted_distances<-distances[order(distances[,1]),]
+sorted_distances
+
+max_distance <-k
