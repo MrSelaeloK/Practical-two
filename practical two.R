1446473ea8b7079e4566f03339ded7f7ee96345c
Selaelo Kgafela <kgfsel001@myuct.ac.za>
2025-02-08 16:11:33 +0200

    calculated beta

--- practical two.R
+++ practical two.R
@@ -20,7 +20,29 @@ for (i in 1:nrow(X)){
   distances[i,1]<-abs(X[1]-X[i])
   distances[i,2]<-X[i]
 }
-sorted_distances<-distances[order(distances[,1]),]
+sorted_distances<-distances[order(distances[,1]),]#sorted by distances
 sorted_distances
 
-max_distance <-k
+max_distance <-max(sorted_distances[1:k,1])
+max_distance
+weights<-matrix(NA,nrow=nrow(X))
+weights[1]
+#determining the weights
+for( i in 1:nrow(X)){
+  if(sorted_distances[i,1]<=max_distance){
+    weights[i]<-(1-(abs(X[1]-sorted_distances[i,2])/max_distance)^3)^3
+  }else{
+    weights[i]<-0
+  }
+}
+intercept<-rep(1,nrow(X))
+X<-as.matrix(cbind(intercept,X))
+
+#calculating Beta coefficients
+Y
+weights_vector<-as.vector(weights)
+Beta_coefficients<-solve(t(X)%*%diag(weights_vector)%*%X)%*%(t(X)%*%diag(weights_vector)%*%Y_matrix)
+Beta_coefficients
+
+
+
