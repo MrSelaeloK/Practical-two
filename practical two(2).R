0710591201e132e46332a1fdfff005313b6445a3
Selaelo Kgafela <kgfsel001@myuct.ac.za>
2025-02-09 14:47:36 +0200

    fixed practical two(R). polishing and inculding of labels on graphs left now

--- practical two(2).R
+++ practical two(2).R
@@ -1,7 +1,7 @@
 #Generating simulated data
 set.seed(1)
 X<-1:100
-error_terms<-rnorm(100,0,02^2)
+error_terms<-rnorm(100,0,0.2)
 Y_matrix<-matrix(error_terms)
 
 for(i in 1:length(X)){
@@ -9,11 +9,11 @@ for(i in 1:length(X)){
   Y_matrix[i]<-sin(X[i]/10)+Y_matrix[i]
 }
 # span 
-k<-ceiling(0.021*length(X))#should be put inside function
+k<-ceiling(0.5*length(X))#should be put inside function
 
 #number of neighbuors determines how much sectioning off is done
 #matrices and vectors
-weights<-rep(NA,
+weights<-rep(0,
                 length(X))
 distances_and_index<-matrix(NA,
                   length(X),ncol=2)
@@ -34,35 +34,35 @@ for(i in 1:length(X)){
   distances_and_index
   sortedDistances_and_index<-distances_and_index[order(distances_and_index[,1]),]
   sortedDistances_and_index
-  #Finding the closest neighbours index and adding to weight to weights
+  #Finding the closest neighbors index and adding to weight to weights
   maxdistance<-sortedDistances_and_index[k+1,1] #may have to fix max distance
   
   #please help here
   #max distance is 2
+  weights
   for(l in 1:length(X)){
     if(sortedDistances_and_index[l,1]<=maxdistance){ #closest index found
       print(sortedDistances_and_index[l,2])
       weights[sortedDistances_and_index[l,2]]<-
-       (1-(abs(X[i]-X[sortedDistances_and_index[l,2]])/maxdistance)^3)^3
-    }else{
-      weights[sortedDistances_and_index[l, 2]] <- 0
+        (1-(abs(X[i]-X[sortedDistances_and_index[l,2]])/maxdistance)^3)^3
     }
+    print((1-(abs(X[i]-X[sortedDistances_and_index[l,2]])/maxdistance)^3)^3)
   }
+  weights
   
   #calculating the beta
-  new_X<-cbind(rep(1,length(X)),X)
+  new_X=cbind(rep(1,length(X)),X)
   new_X
   Beta_coefficients<-solve(t(new_X)%*%diag(weights)%*%new_X)%*%(t(new_X)%*%diag(weights)%*%Y_matrix)
   Beta_coefficients
   
   lowess[i]<-Beta_coefficients[1]+Beta_coefficients[2]*X[i]
-  
-  }
+ }
 lowess
 
-output<-lowess(X,Y_matrix,0.051)
+output<-lowess(X,Y_matrix,0.50)
 output
-plot(X, Y_matrix, main = "LOWESS Smoothing", xlab = "X", ylab = "Y", pch = 16)
-lines(lowess, col = "red", lwd = 2) 
+plot(X, Y_matrix, main = "Lowess Smoothing", xlab = "X", ylab = "Y", pch = 16)
+lines(lowess, col = "red",lwd=2) 
 lines(output,col="Blue",lwd=2)
 
